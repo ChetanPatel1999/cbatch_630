9375ccfb155d836ef58d965a0ba4f1f7acfc915d
jagdish575 <jagdishprajapati573@gmail.com>
2024-05-07 19:35:05 +0530

    07/05

---
+++ add.c
@@ -0,0 +1,13 @@
+//wap to add two numbers .
+#include<stdio.h>
+void main()
+{
+	float num1,num2,sum;//variable declare 
+	num1=10.9;
+	num2=90.7;
+	printf("this is addition programm :\n");
+	printf("value of num1 : %.2f\n",num1);
+	printf("value of num2 : %.2f\n",num2);
+	sum=num1+num2;
+	printf("sum of %.2f and %.2f : %.2f\n",num1,num2,sum);	
+}
---
+++ frist.c
@@ -0,0 +1,14 @@
+//use of formate specifire
+#include<stdio.h>
+void main()
+{
+	printf("integer value = %d\n",12+5);
+	printf("float value = %.2f\n",12.52);
+	printf("charcter value =%c\n",'A');
+	char ch;
+	ch='a';
+	printf("value of ch = %c\n",ch);
+	printf("ascii value of %c = %d\n",ch,ch);
+	printf("ascii value of %c = %d\n",'b','b');
+		
+}
